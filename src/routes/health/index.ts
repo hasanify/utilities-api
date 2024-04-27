4bc250c260c50dbb3e2c2068aebb3f893e42a3ac
hasanify <hasanali.abps@gmail.com>
2024-04-27 20:31:47 +0530

    update health endpoint

--- src/routes/health/index.ts
+++ src/routes/health/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  return res.sendStatus(200);
+  return res.status(200).json({ msg: "ok" });
 });
 
 export default router;
