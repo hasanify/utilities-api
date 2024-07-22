544aa5090bf6f4571144f5a59829057edc6e8827
hasanify <hasanali.abps@gmail.com>
2024-07-22 15:13:21 +0530

    misc

--- src/routes/health/index.ts
+++ src/routes/health/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  return res.sendStatus(200);
+  return res.json({ status: "ok" });
 });
 
 export default router;
